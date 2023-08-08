2c754c8011862b0fa02fa431a550d05ca530e433
Mareike Boßelmann <mareike.bosselmann@gmx.de>
2023-08-08 15:13:09 +0200

    Add focus for world element as fix for codesandbox issue

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 import { handleStart } from "./js/runGame.js";
 import { setPixelToWorldScale } from "./js/setPixelToWorldScale.js";
 
+const world = document.querySelector('[data-js="world"]');
 setPixelToWorldScale();
 window.addEventListener("resize", setPixelToWorldScale);
 document.addEventListener("keydown", handleStart, { once: true });
+world.focus();
