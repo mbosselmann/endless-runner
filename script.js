c164802b2a80b0560f03b6b7eb8d8dc0088f5e18
Mareike Boßelmann <mareike.bosselmann@gmx.de>
2023-08-08 16:51:24 +0200

    Remove pixel to world scale functionality

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 import { handleStart } from "./js/runGame.js";
-import { setPixelToWorldScale } from "./js/setPixelToWorldScale.js";
 
 const world = document.querySelector('[data-js="world"]');
-setPixelToWorldScale();
-window.addEventListener("resize", setPixelToWorldScale);
 document.addEventListener("keydown", handleStart, { once: true });
 world.focus();
