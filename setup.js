ddbd3351bab5ca3ee4ab302670867388f0e5a266
Mareike Boßelmann <mareike.bosselmann@gmx.de>
2023-08-14 12:30:56 +0200

    add comments about ideas

--- setup.js
+++ setup.js
@@ -5,3 +5,13 @@ export const figure = {
     "https://api.dicebear.com/6.x/thumbs/svg?seed=Annie?faceOffsetX=-15&eyes=variant3W12",
   lose: "https://api.dicebear.com/6.x/thumbs/svg?seed=Boots&shapeRotation=0&faceOffsetX=-15",
 };
+
+// ideas:
+// decide about obstacles?
+// decide about text that is displayed if you lose?
+// decide about grounds?
+// custom colors?
+// decide for a color theme? light & dark? or just one?
+// change speedrate?
+// change jump height?
+// deactivate rotation if you want?
