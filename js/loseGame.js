48d98a3a4153c6e98b8b6b3b0b6093b4b07ecd8a
Mareike Boßelmann <mareike.bosselmann@gmx.de>
2023-08-21 17:42:50 +0200

    Update handle lose functionality

--- js/loseGame.js
+++ js/loseGame.js
@@ -7,7 +7,6 @@ import {
 } from "./updateCustomProperty.js";
 
 const startScreen = document.querySelector('[data-js="start-screen"]');
-const player = document.querySelector('[data-js="player"]');
 const text = document.querySelector('[data-js="text"]');
 const world = document.querySelector('[data-js="world"]');
 
@@ -36,21 +35,22 @@ function isCollision(rectangle1, rectangle2) {
 }
 
 export function handleLose() {
-  setPlayerLose();
-
   text.classList.remove("hide");
+  const player = document.querySelector('[data-js="player"]');
+
   const playerBottom = getCustomProperty(player, "--bottom");
   const playerLeft = getCustomProperty(player, "left");
   const playerWidth = getCustomProperty(player, "width");
   const worldHeight = getCustomProperty(world, "height");
+  setPlayerLose();
 
   setCustomProperty(
     text,
     "--bottom",
-    Math.floor((worldHeight / 100) * playerBottom + 120) + "px"
+    Math.floor((worldHeight / 100) * playerBottom + 130) + "px"
   );
 
-  setCustomProperty(text, "--left", playerLeft + playerWidth + 20 + "px");
+  setCustomProperty(text, "--left", playerLeft + playerWidth + 120 + "px");
   setTimeout(() => {
     document.addEventListener("keydown", handleStart, { once: true });
     startScreen.classList.remove("hide");
