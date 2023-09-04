80846c49cf1b42bbc274d7229003eca299040324
Mareike Boßelmann <mareike.bosselmann@gmx.de>
2023-09-04 12:15:41 +0200

    Adjust speed scale to world size

--- .where-the-magic-is/js/gameLogic/speedScale.js
+++ .where-the-magic-is/js/gameLogic/speedScale.js
@@ -1,9 +1,21 @@
+import { getCustomProperty } from "../helper/updateCustomProperty.js";
+
 const SPEED_SCALE_INCREASE = 0.00005;
 
 export let speedScale;
 
 export function setupSpeedScale() {
-  speedScale = 2;
+  const world = document.querySelector('[data-js="world"]');
+  const worldWidth = getCustomProperty(world, "width");
+  if (worldWidth >= 800) {
+    return (speedScale = 1);
+  }
+  if (worldWidth >= 500) {
+    return (speedScale = 1.5);
+  }
+  if (worldWidth < 500) {
+    return (speedScale = 2);
+  }
 }
 
 export function updateSpeedScale(delta) {
