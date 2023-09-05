9a718986202ac056c2bfcf64ee6aabeff5489035
Mareike Boßelmann <mareike.bosselmann@gmx.de>
2023-09-05 23:21:50 +0200

    Add example JS code

--- setup.js
+++ setup.js
@@ -1,30 +1,10 @@
-// setup your figure for the game
-export const figure = {
-  run1: "/assets/fw_1.png",
-  run2: "/assets/fw_2.png",
-  jumping: "/assets/fj.png",
-  lose: "/assets/fh.png",
+export const player = {
+  run1: "/assets/gw_1.png",
+  run2: "/assets/gw_2.png",
+  jumping: "/assets/gj.png",
+  lose: "/assets/gh.png",
   rotate: false,
   jumpToTheMoon: false,
 };
 
-// decide for a theme: night or bw
-// export const theme = "night";
-
-// decide for a custom color set
-// export const customColors = {
-//   textColor: "midnightblue",
-//   textBackgroundColor: "papayawhip",
-//   skyColor: "lightsalmon",
-//   gameBackgroundColor: "midnightblue",
-// };
-
-// which text is displayed if you lose? -> change in HTML!?
-
-// ideas:
-// decide about obstacles?
-// decide about grounds?
-// custom colors?
-// decide for a color theme? light & dark? or just one?
-// change speedrate?
-// change jump height?
+export const theme = "night";
